0b5c9ac2d35c8e3db3535e4f273782d00b7cdcc0
Mike Pokalsky <mpoksky1989@gmail.com>
2020-07-25 15:25:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function doToElementsInArray(array, callback) {
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
- 
+
 var animals = ["dog", "cat", "squirrel"];
